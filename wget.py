cd7188735bb8ae693afdebe3f0ed64b257ad1cac
crhysc <149001340+crhysc@users.noreply.github.com>
2025-01-16 20:41:50 -0500

    Update wget.py

--- wget.py
+++ wget.py
@@ -1,10 +1,12 @@
-'id_prop_path': "Out/id_prop.csv",
- 'prefix': 'atomgpt_run',
- 'model_name': "knc6/atomgpt_mistral_tc_supercon",
- 'batch_size': 2,
- 'num_epochs': 5,
- 'seed_val': 42,
- 'num_train': 2,
- 'num_val': 2,
- 'num_test': 2,
- 'model_save_path': 'lora_model_m'
+{
+  'id_prop_path': "/users/crc00042/SCRATCH/testAtomGPT/id_prop_test/id_prop.csv",
+  'prefix': 'atomgpt_run',
+  'model_name': "knc6/atomgpt_mistral_tc_supercon",
+  'batch_size': 2,
+  'num_epochs': 5,
+  'seed_val': 42,
+  'num_train': 2,
+  'num_val': 2,
+  'num_test': 2,
+  'model_save_path': '/users/crc00042/SCRATCH/testAtomGPT/out'
+}
