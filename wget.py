0ff090e5ffd339588a7b894496748741356292df
crhysc <149001340+crhysc@users.noreply.github.com>
2025-01-16 20:38:45 -0500

    Update wget.py

--- wget.py
+++ wget.py
@@ -1,47 +1,10 @@
-#!/bin/bash
-
-
-#SBATCH -J test_run
-#name of job
-
-#SBATCH -o output.out
-#standard output file
-
-#SBATCH --nodelist=dscog027
-
-#SBATCH --gres=gpu:1
-
-#SBATCH -n 1
-#number of tasks
-
-#SBATCH -p gpu_7day
-#queue for the job
-
-#SBATCH -t 8:00:00
-
-. $HOME/bashrc
-
-conda init
-conda activate mesaverde
-
-nvidia-smi
-
-echo ""
-echo "<--- Job output --->"
-echo ""
-
-# Put job commands here V
-
-python forward_prediction.py ~/3DPBE/outforward/config.json ~/3DPBE/id_prop/POSCAR__jsonNum_0_entryNum100000_agm003704385.vasp
-
-# Put job commands above ^
-
-echo ""
-echo "{JOB FINISHED}"
-echo ""
-echo "Job ID:              $SLURM_JOB_ID"
-echo "Job Name:            $SLURM_JOB_NAME"
-echo "Number of Nodes:     $SLURM_JOB_NUM_NODES"
-echo "Number of CPU cores: $SLURM_CPUS_ON_NODE"
-echo "Number of Tasks:     $SLURM_NTASKS"
-echo "Partition:           $SLURM_JOB_PARTITION"
+'id_prop_path': "Out/id_prop.csv",
+ 'prefix': 'atomgpt_run',
+ 'model_name': "knc6/atomgpt_mistral_tc_supercon",
+ 'batch_size': 2,
+ 'num_epochs': 5,
+ 'seed_val': 42,
+ 'num_train': 2,
+ 'num_val': 2,
+ 'num_test': 2,
+ 'model_save_path': 'lora_model_m'
